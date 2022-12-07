b1ee9550f1ddd95540ad06c59919ff31a856d9f5
aysuakbaba <74005918+aysuakbaba@users.noreply.github.com>
2022-12-08 02:01:09 +0300

    Delete app.js

--- OdinProjects/MemeGenerator/app.js
+++
@@ -1,8 +0,0 @@
-const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=MfjL7knDifKQQM2kDzdQFbTnLHCl32nM&s=films',{mode:"cors"})
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(response){
-        img.src = response.data.images.original.url;
-    });
